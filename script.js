0b7cd7d4b7f1f07648fcce01a2ec0446b7e1d6d6
Maxime Authié <authie.maxime@orange.fr>
2023-01-27 21:40:19 +0100

    Finalisation de la barre de recherche et test du fond

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 // Récupérer tous les éléments du DOM modifiables
-let city            = document.querySelector('#city');
+let citySearch      = 'Toulouse';
+let cityDisplay     = document.querySelector('#city-display');
 let temp            = document.querySelector('#temp');
 let tempMin         = document.querySelector('#temp-min');
 let tempFeelsLike   = document.querySelector('#feels-like');
 let pressure        = document.querySelector('#pressure');
 let humidity        = document.querySelector('#humidity');
 let windSpeed       = document.querySelector('#wind');
+let button          = document.querySelector('#magnifying-glass');
 
 
 //Appel de l'API
 let majData = async () => {
-    let data = await fetch('https://api.openweathermap.org/data/2.5/weather?q=toulouse&appid=efc0b67a43b8a0ef061f770e6ba10b53&units=metric');
-    let dataTransformed = await data.json();
-    temp.innerText              = dataTransformed.main.temp+"°C";
-    tempMin.innerText           = dataTransformed.main.temp_min;
-    tempFeelsLike.innerText     = dataTransformed.main.feels_like;
-    pressure.innerText          = dataTransformed.main.pressure;
-    humidity.innerText          = dataTransformed.main.humidity;
+    citySearch              = document.querySelector('#city-search');
+    let data                = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + citySearch.value + '&appid=&units=metric'); //mettre la clé
+    let dataTransformed     = await data.json();
+    let weather             = dataTransformed.weather[0].main;
+
+    cityDisplay.innerText       = dataTransformed.name;
+    temp.innerText              = dataTransformed.main.temp + "°C";
+    tempMin.innerText           = dataTransformed.main.temp_min + "°C";
+    tempFeelsLike.innerText     = dataTransformed.main.feels_like + "°C";
+    pressure.innerText          = dataTransformed.main.pressure + "Pa";
+    humidity.innerText          = dataTransformed.main.humidity + "%";
     windSpeed.innerText         = dataTransformed.wind.speed;
+    citySearch.value ='';
+
+    if (weather=="Clear") {
+        document.body.style.backgroundImage = 'url("medias/day/clear.jpg")';
+    }
 }
 
-majData();
+
+button.addEventListener('click', majData);
