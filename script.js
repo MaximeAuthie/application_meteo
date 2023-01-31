6716a16e0421eb9bf90a680867a45c43fe9decac
Maxime Authié <authie.maxime@orange.fr>
2023-01-31 19:22:35 +0100

    Ajout de la géolocalisation, changement des donnée en appuyant sur entrée

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Récupérer tous les éléments du DOM modifiables
-let citySearch      = 'Toulouse';
+let citySearch      = '';
 let cityDisplay     = document.querySelector('#city-display');
 let img             = document.querySelector('#icon');
 let temp            = document.querySelector('#temp');
@@ -9,26 +9,10 @@ let pressure        = document.querySelector('#pressure');
 let humidity        = document.querySelector('#humidity');
 let windSpeed       = document.querySelector('#wind');
 let button          = document.querySelector('#magnifying-glass');
+let form            = document.querySelector('#city-search');
 
-//Appel de l'API
-let majData = async () => {
-    citySearch              = document.querySelector('#city-search');
-    let data                = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + citySearch.value + '&appid=&units=metric'); //mettre la clé
-    let dataTransformed     = await data.json();
-    let weather             = dataTransformed.weather[0].main;
-    let icon                = dataTransformed.weather[0].icon;
-    let dayOrNight          = icon[2];
-
-    cityDisplay.innerText       = dataTransformed.name;
-    temp.innerText              = dataTransformed.main.temp + "°C";
-    tempMin.innerText           = dataTransformed.main.temp_min + "°C";
-    tempFeelsLike.innerText     = dataTransformed.main.feels_like + "°C";
-    pressure.innerText          = dataTransformed.main.pressure + "Pa";
-    humidity.innerText          = dataTransformed.main.humidity + "%";
-    windSpeed.innerText         = dataTransformed.wind.speed + "km/h";
-    img.setAttribute('src', "http://openweathermap.org/img/wn/" + icon + "@4x.png" );
-    citySearch.value ='';
-
+// Fonction permettant de changer le background de body
+function bodybackground (dayOrNight, weather) {
     if (dayOrNight == 'd') {
         switch (weather) {
             case 'Clear':
@@ -86,5 +70,67 @@ let majData = async () => {
     }
 }
 
+// Fonction en cas d'échec de la géolocalisation
+function geolocationError () {
+    citySearch = 'Paris';
+    majData(citySearch);
+}
 
-button.addEventListener('click', majData);
+//Appel de l'API en géolocalisant l'utilisateur
+if ("geolocation" in navigator) {
+    navigator.geolocation.watchPosition( async (position) => {
+        let data                = await fetch('https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat='+ position.coords.latitude + '&appid=efc0b67a43b8a0ef061f770e6ba10b53&units=metric'); //mettre la clé
+        console.log('https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat='+ position.coords.latitude + '&appid=efc0b67a43b8a0ef061f770e6ba10b53&units=metric');
+        let dataTransformed     = await data.json();
+        let weather             = dataTransformed.weather[0].main;
+        let icon                = dataTransformed.weather[0].icon;
+        let dayOrNight          = icon[2];
+
+        cityDisplay.innerText       = dataTransformed.name;
+        temp.innerText              = dataTransformed.main.temp + "°C";
+        tempMin.innerText           = dataTransformed.main.temp_min + "°C";
+        tempFeelsLike.innerText     = dataTransformed.main.feels_like + "°C";
+        pressure.innerText          = dataTransformed.main.pressure + "Pa";
+        humidity.innerText          = dataTransformed.main.humidity + "%";
+        windSpeed.innerText         = dataTransformed.wind.speed + "km/h";
+        img.setAttribute('src', "http://openweathermap.org/img/wn/" + icon + "@4x.png" );
+        citySearch.value ='';
+
+   bodybackground(dayOrNight,weather);
+    }, geolocationError);
+}
+
+//Appel de l'API lors du click de recherche
+let majData = async (citySearch) => {
+    city             = citySearch;
+    let data                = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=efc0b67a43b8a0ef061f770e6ba10b53&units=metric'); //mettre la clé
+    let dataTransformed     = await data.json();
+    let weather             = dataTransformed.weather[0].main;
+    let icon                = dataTransformed.weather[0].icon;
+    let dayOrNight          = icon[2];
+
+    cityDisplay.innerText       = dataTransformed.name;
+    temp.innerText              = dataTransformed.main.temp + "°C";
+    tempMin.innerText           = dataTransformed.main.temp_min + "°C";
+    tempFeelsLike.innerText     = dataTransformed.main.feels_like + "°C";
+    pressure.innerText          = dataTransformed.main.pressure + "Pa";
+    humidity.innerText          = dataTransformed.main.humidity + "%";
+    windSpeed.innerText         = dataTransformed.wind.speed + "km/h";
+    img.setAttribute('src', "http://openweathermap.org/img/wn/" + icon + "@4x.png" );
+    citySearch.value ='';
+
+   bodybackground(dayOrNight,weather);
+}
+
+button.addEventListener('click', () => {
+    citySearch = document.querySelector('#city-search');
+    majData(citySearch.value);
+} );
+
+form.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        citySearch = document.querySelector('#city-search');
+        majData(citySearch.value);
+    }
+});
